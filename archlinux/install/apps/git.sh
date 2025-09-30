7413ee93b03a97bf932fa56dd2620413d4a5e503
Valentin Brosseau <c4software@gmail.com>
2025-09-30 22:17:21 +0200

    Fix order and config folders

--- archlinux/install/apps/git.sh
+++ archlinux/install/apps/git.sh
@@ -1,14 +1,21 @@
 
 function setup(){
+    mkdir -p ~/.config/git
+    touch ~/.config/git/config
+
     # Définition des alias git par defaut
     git config --global alias.co checkout
     git config --global alias.br branch
     git config --global alias.ci commit
     git config --global alias.st status
     git config --global pull.rebase true
-    git config --global init.defaultBranch master
+    git config --global init.defaultBranch main
 }
 
 function check(){
-    return
+    if [ ! -f ~/.config/git/config ]; then
+        show_error "Git configuration file not found."
+    else
+        show_success "Git configuration"
+    fi
 }
