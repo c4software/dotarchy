f55f7d9e7a70ca2269af5b9b098c0250cf0ffa75
Valentin Brosseau <c4software@gmail.com>
2025-10-05 23:12:03 +0200

    Added an icon to waybar when the notifications are silenced

---
+++ archlinux/install/hyprland/config/waybar/indicators/do-not-disturb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_mode=$(makoctl mode | grep "do-not-disturb")
+
+if [[ "$current_mode" == "do-not-disturb" ]]; then
+  echo '{"text": "", "tooltip": "All notifications are silenced", "class": "active"}'
+else
+  echo '{"text": ""}'
+fi
