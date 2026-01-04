4034c04d74f37345bbdb4a5143bb160e93b18316
Valentin Brosseau <c4software@gmail.com>
2026-01-04 23:51:47 +0100

    Correction du texte affiché pour l'état VPN déconnecté dans le script WireGuard

--- archlinux/install/tilling/config/waybar/scripts/wireguard.sh
+++ archlinux/install/tilling/config/waybar/scripts/wireguard.sh
@@ -3,7 +3,7 @@
 interfaces=$(ip -o link show type wireguard | awk -F': ' '{print $2}' | cut -d@ -f1)
 
 if [ -z "$interfaces" ]; then
-  echo '{"text":"󱙱 VPN off","class":"disconnected","icon":""}'
+  echo '{"text":"󱙱 VPN","class":"disconnected","icon":""}'
 else
   text=$(echo "${interfaces^}" | tr '\n' ' ' | sed 's/ $//')
   echo "{\"text\":\"󰌾 $text\",\"class\":\"connected\",\"icon\":\"\"}"
