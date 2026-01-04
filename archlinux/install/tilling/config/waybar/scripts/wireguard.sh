36834d0678adfe597bf077bf26d75c73e409632e
Valentin Brosseau <c4software@gmail.com>
2026-01-04 23:22:07 +0100

    Ajout de règles pour gérer les configurations WireGuard et mise à jour des scripts associés.

---
+++ archlinux/install/tilling/config/waybar/scripts/wireguard.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+interfaces=$(ip -o link show type wireguard | awk -F': ' '{print $2}' | cut -d@ -f1)
+
+if [ -z "$interfaces" ]; then
+  echo '{"text":"󱙱 VPN off","class":"disconnected","icon":""}'
+else
+  text=$(echo "${interfaces^}" | tr '\n' ' ' | sed 's/ $//')
+  echo "{\"text\":\"󰌾 $text\",\"class\":\"connected\",\"icon\":\"\"}"
+fi
