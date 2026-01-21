43620b9279c97a35fd40873e1617de9355f61269
Valentin Brosseau <c4software@gmail.com>
2026-01-21 16:37:46 +0100

    Ajout de l'indicateur "Ne pas déranger" et mise à jour du style CSS

--- archlinux/install/tilling/config/waybar/indicators/do-not-disturb.sh
+++ archlinux/install/tilling/config/waybar/indicators/do-not-disturb.sh
@@ -5,5 +5,5 @@ current_mode=$(makoctl mode | grep "do-not-disturb")
 if [[ "$current_mode" == "do-not-disturb" ]]; then
   echo '{"text": "", "tooltip": "All notifications are silenced", "class": "active"}'
 else
-  echo '{"text": ""}'
+  echo '{"text": "", "tooltip": "Notifications are enabled", "class": "inactive"}'
 fi
