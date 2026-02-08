644e2b18d96bab806adcfaff3f55a7ff668ee884
Valentin Brosseau <c4software@gmail.com>
2026-02-08 21:38:00 +0100

    Remove extra onlyoffice since the package is no up to date on the AUR repo

--- archlinux/install/desktop/applications.sh
+++ archlinux/install/desktop/applications.sh
@@ -2,24 +2,6 @@
 
 function setup(){
     mkdir -p "~/Applications"
-    wget -O ~/Applications/OnlyOffice.AppImage "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/DesktopEditors-x86_64.AppImage"
-    chmod +x ~/Applications/OnlyOffice.AppImage
-
-    # Create desktop shortcut
-    cat <<EOF > ~/.local/share/applications/OnlyOffice.desktop
-[Desktop Entry]
-Type=Application
-Name=ONLYOFFICE Desktop Editors
-GenericName=Office Suite
-Comment=Edit office documents
-Exec=/home/vbrosseau/Applications/OnlyOffice.AppImage %U
-Icon=onlyoffice
-Terminal=false
-StartupWMClass=ONLYOFFICE
-Categories=Office;WordProcessor;Spreadsheet;Presentation;
-MimeType=application/msword;application/vnd.openxmlformats-officedocument.wordprocessingml.document;application/vnd.oasis.opendocument.text;application/vnd.ms-excel;application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;application/vnd.oasis.opendocument.spreadsheet;application/vnd.ms-powerpoint;application/vnd.openxmlformats-officedocument.presentationml.presentation;application/vnd.oasis.opendocument.presentation;
-EOF
-
     # Create quickcalc rofi entry
     # ~/.local/share/applications/quickcal.desktop
     cat <<EOF > ~/.local/share/applications/quickcal.desktop
@@ -36,13 +18,6 @@ EOF
 }
 
 function check() {
-    # Check if OnlyOffice AppImage exists
-    if [ -f "$HOME/Applications/OnlyOffice.AppImage" ]; then
-        show_success "OnlyOffice AppImage"
-    else
-        show_error "OnlyOffice AppImage" "The OnlyOffice AppImage is missing in ~/Applications/."
-    fi
-
     # Check if quickcal desktop entry exists
     if [ -f "$HOME/.local/share/applications/quickcal.desktop" ]; then
         show_success "quickcal desktop entry"
